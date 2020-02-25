294759e2f473b22c91b79bd684e45d2b21cf64ad
Claudine <clo.pradal@gmail.com>
2020-02-25 09:16:34 +0100

    change config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # get '/blog', to: redirect('http://redirection.mihivai-blog.com/'), as: 'blog'
+  get '/blog', to: redirect('https://test-redirection-fev.herokuapp.com/blog/'), as: 'blog'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '(*path)', to: 'application#blog', constraints: { subdomain: 'blog' }
 end
