aef1f23eabff116cc5592fb7718e5bcdafe37e8f
Claudine <clo.pradal@gmail.com>
2020-04-23 08:37:36 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get '/blog', to: redirect('https://test-redirection-fev.herokuapp.com/blog/', status: 301)
+  get '/blog/.', to: redirect('https://test-redirection-fev.herokuapp.com/blog/.', status: 301)
+  # get '/blog', to: redirect('https://test-redirection-fev.herokuapp.com/blog')
   # get '/blog', to: redirect('http://blog.mihivai-blog.com/'), as: 'blog'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
