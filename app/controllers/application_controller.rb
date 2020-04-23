8f2318c288296b6ffe1aca1fab4f2f1244b0505e
Claudine <clo.pradal@gmail.com>
2020-04-23 08:52:04 +0200

    update for all blog

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def blog
+    redirect_to "https://test-redirection-fev.herokuapp.com/blog#{request.fullpath.gsub('/blog','')}", :status => :moved_permanently
+  end
 end
